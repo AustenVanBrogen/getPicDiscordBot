947c2a8255f2fe7ac041420215907414cad0eb62
Austen Van Brogen <43625899+AustenVanBrogen@users.noreply.github.com>
2022-01-18 17:59:36 -0600

    Changed the program to use promises to wait for the pictures

--- index.js
+++ index.js
@@ -3,46 +3,79 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const fs = require('fs');
 const https = require('https');
+const { promisify } = require('util');
 
 let keys = JSON.parse(fs.readFileSync('keys.json'));
 let botToken = keys.botToken;
 const prefix = '!';
 
-const options = {
-    hostname: 'dog.ceo',
+function showPicture(pictureJSON)
+{
+  console.log(pictureJSON.message);
+}
+
+function buildOptions(hostnameVar, pathVar)
+{
+  let options = {
+    hostname: hostnameVar,
     port: 443,
-    path: '/api/breeds/image/random',
+    path: pathVar,
     method: 'GET'
-};
+  };
+  return options;
+}
+
+function getPicture(message, command, options)
+{
+  let response;
+  return new Promise(function (resolve, reject){
+    const req = https.request(options, res => {
+      console.log(`statusCode: ${res.statusCode}`);
+    
+      res.on('data', d => {
+        // message.channel.send(`Here's a ${command} picture`);
+        // message.channel.send(JSON.parse(d).message);
+        response = JSON.parse(d);
+        //console.log(JSON.parse(d));
+        resolve(response);
+      });
+    });
+    
+    req.on('error', error => {
+      console.error(error);
+      reject(error);
+    });
+    
+    req.end();
+  });
+}
 
 client.once('ready', () => {
     console.log("getPicureBot is online")
 });
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async (message) => {
     if(message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split("/ +/");
     const command = args.shift().toLowerCase();
+    let hostname;
+    let path;
 
     if(command === 'dog')
     {
-        message.channel.send("Here's a dog picture");
+      hostname = 'dog.ceo';
+      path = '/api/breeds/image/random';
+      getPicture(message, command, buildOptions(hostname, path))
+      .then(response => message.channel.send(response.message))
+      .catch(error => console.log(error));
 
-        const req = https.request(options, res => {
-            console.log(`statusCode: ${res.statusCode}`);
-          
-            res.on('data', d => {
-              //console.log(JSON.parse(d));
-              message.channel.send(JSON.parse(d).message);
-            });
-          });
-          
-          req.on('error', error => {
-            console.error(error);
-          });
-          
-          req.end();
+      // message.channel.send("Here's a dog picture");
+      // message.channel.send(JSON.parse(response).message);
+    }
+    else if(command === 'cat')
+    {
+
     }
 });
 
